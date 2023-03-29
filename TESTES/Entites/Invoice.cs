9edd9ecd69340254af354f9af367dcadb4c49cdd
Alesf Luiz <alesfluiz@gmail.com>
2023-03-29 17:33:17 -0300

    add invoice

---
+++ TESTES/Entites/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace AgendaDesign.Entites
+{
+    internal class Invoice
+    {
+        public double Payment { get; set; }
+
+        public Invoice(double payment)
+        {
+            Payment = payment;
+        }
+
+        public double TotalPayment(double valouPerProcedure)
+        {
+            Payment += valouPerProcedure;
+            return Payment;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
